f10169e26bf4e4d8213344d71f3a716aa6dbb46c
Amol Jaikar <amol@opennetworking.org>
2023-03-03 09:48:18 -0500

    Credit card verification program

---
+++ CreditCard.py
@@ -0,0 +1,56 @@
+#-------------------------------------------------------------------------------
+# Credit card verification program
+# Criteria: 
+#	1. First number must be 4, 5 or 6
+#	2. Must contain exactly 16 digits 
+#	3. May have characyers but not other than "-" and seperated by 4 numbers
+#	4. Consecutive 4 numbers must not be same 
+#-------------------------------------------------------------------------------
+
+#--function to calculate consecutive 4 identical--
+def Identical4(number):
+    for i in range(0,len(number)-3): 
+        if number[i]==number[i+1]:
+           if number[i+1]==number[i+2]:
+              if number[i+2]==number[i+3]:
+                 return 0
+    return 1
+
+
+#--function to varify the card-- 
+def Varify(card_number):
+ #--condition 1--                 
+ if card_number[0]!='4' and card_number[0]!='5' and card_number[0]!='6':
+   print(card_number+": invalid1")
+ #--condition 2--
+ elif card_number.isdigit()==False:
+   if card_number.find("-")>0:
+      if card_number.count('-')==3:
+         if card_number[4]=='-' and card_number[9]=='-' and card_number[14]=='-':
+            card_number = card_number.replace("-","")
+            if Identical4(card_number)==0:
+               print(card_number+": invalid2")
+            else:
+               print(card_number+": valid1") 
+         else:
+            print(card_number+": invalid3")
+      else:
+         print(card_number+": invalid4")
+   else:
+      print(card_number+": invalid5")
+ else:
+   if len(card_number)!=16:
+      print(card_number+": invalid6")
+   elif Identical4(card_number)==0:
+      print(card_number+": invalid7")
+   else: 
+      print(card_number+": valid2")
+
+
+#--Sample input--
+Varify("4123456789123456")
+Varify("5123-4567-8912-3456")
+Varify("61234-567-8912-3456")
+Varify("4123356789123456")
+Varify("5133-3367-8912-3456")
+Varify("5123 - 3567 - 8912 - 3456")
